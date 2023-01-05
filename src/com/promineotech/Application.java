fdd3d4d1c69f309a9336450df94e52c2013dc0ba
Adam Salt <salt48186@gmail.com>
2023-01-05 10:15:28 -0800

    finished week1 labs

--- src/com/promineotech/Application.java
+++ src/com/promineotech/Application.java
@@ -5,18 +5,34 @@ public class Application {
 	public static void main(String[] args) {
 		int availablePlaneSeats = 8;
 		double costOfGroceries = 174.85;
-		char middleInitial = 'C';
+		char middleInitial = 'H';
 		boolean isHotOutside = false;
 		String firstName = "Adam";
 		String streetAddress = "1234 Main St";
 		
-		System.out.println(availablePlaneSeats);
-		System.out.println(costOfGroceries);
-		System.out.println(middleInitial);
-		System.out.println(isHotOutside);
-		System.out.println(firstName);
-		System.out.println(streetAddress);
-
+		System.out.println("Available seats left on the plane:" + " " + availablePlaneSeats); //uses " " to concatenate and add spaces to sentences
+		System.out.println("$" + costOfGroceries + " " + "is the cost of this week's groceries");//uses " " to concatenate and add spaces to sentences
+		System.out.println("My middle initial is:" + middleInitial);
+		System.out.println(isHotOutside + "," + "it is not hot outside" );
+		System.out.println(firstName + " is my first name");// uses spaces at the beginning of string to add spaces to sentences
+		System.out.println(streetAddress + " is the address of the best pizza place in town");// uses spaces at the beginning of string to add spaces to sentences
+        
+		availablePlaneSeats -= 2;
+		System.out.println("Available seats left on the plane:" + " " + availablePlaneSeats);
+		costOfGroceries += 2.15;
+		System.out.println("$" + costOfGroceries + " " + "is the cost of this week's groceries");
+		
+		middleInitial = 'C';
+		System.out.println("My middle initial is: " + middleInitial);
+		//isHotOutside = true;
+		isHotOutside = !isHotOutside;
+		System.out.println(isHotOutside + "," + " it is hot outside" );
+		
+	    String fullName = firstName + " " + middleInitial + " Salt";
+	    System.out.println(fullName);
+	    
+	    System.out.println("Hello, my name is " + fullName + "," + " " + "and I live at " + streetAddress);
+	    
 	}
 
 }
