d6c79463466f0188921996bc96679a949fbc30cc
Adam Salt <salt48186@gmail.com>
2023-01-05 09:38:20 -0800

    first commit

---
+++ src/com/promineotech/Application.java
@@ -0,0 +1,22 @@
+package com.promineotech;
+
+public class Application {
+
+	public static void main(String[] args) {
+		int availablePlaneSeats = 8;
+		double costOfGroceries = 174.85;
+		char middleInitial = 'C';
+		boolean isHotOutside = false;
+		String firstName = "Adam";
+		String streetAddress = "1234 Main St";
+		
+		System.out.println(availablePlaneSeats);
+		System.out.println(costOfGroceries);
+		System.out.println(middleInitial);
+		System.out.println(isHotOutside);
+		System.out.println(firstName);
+		System.out.println(streetAddress);
+
+	}
+
+}
